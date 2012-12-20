b0470fcffa6811757b734d2ebef72e6f3b1dc719
Eriks Remess <eriks@remess.lv>
2012-12-20 05:06:22 +0200

    storefronts

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 var getArtwork = require('./lib/itunes-artwork');
-getArtwork(512505156, function(error, artwork){ // "Bad Ass" https://itunes.apple.com/lv/movie/bad-ass/id512505156
+getArtwork(512505156, "US", function(error, artwork){ // "Bad Ass" https://itunes.apple.com/us/movie/bad-ass/id512505156
 	if(!error){
 		console.log(artwork);
 	} else {
