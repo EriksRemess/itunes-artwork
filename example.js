6c894c4109a15e24f20a061633738361319a46c1
Eriks Remess <eriks@remess.lv>
2012-12-29 03:24:41 +0200

    cleanup

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
-var getArtwork = require('./lib/itunes-artwork');
-getArtwork(512505156, "US", function(error, artwork){ // "Bad Ass" https://itunes.apple.com/us/movie/bad-ass/id512505156
+var getArtwork = require('./itunes-artwork');
+getArtwork(512505156, "LV", function(error, artwork){ // "Bad Ass" https://itunes.apple.com/lv/movie/bad-ass/id512505156
 	if(!error){
 		console.log(artwork);
 	} else {
